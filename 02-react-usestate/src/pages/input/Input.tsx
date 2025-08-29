50a49508ce1df3495a4e2fae991b05054250c6fd
AbuTokio <bunyaminxkara@gmail.com>
2025-08-29 15:58:44 +0200

    little fix on input boxes

--- 02-react-usestate/src/pages/input/Input.tsx
+++ 02-react-usestate/src/pages/input/Input.tsx
@@ -8,9 +8,9 @@ export default function Input() {
     <>
       <div className="flex justify-between items-start flex-col gap-8 w-[30vw] p-8 border bg-gray-500">
         <div className="flex justify-center items-center gap-4 w-full">
-          <input className="bg-white p-2" type="text" onChange={(e) => setFirstname(e.target.value)} />
-          <input className="bg-white p-2" type="text" onChange={(e) => setLastname(e.target.value)} />
-          <input className="bg-white p-2" type="email" onChange={(e) => setEmail(e.target.value)} />
+          <input className="bg-white p-2 w-1/3" type="text" onChange={(e) => setFirstname(e.target.value)} />
+          <input className="bg-white p-2 w-1/3" type="text" onChange={(e) => setLastname(e.target.value)} />
+          <input className="bg-white p-2 w-1/3" type="email" onChange={(e) => setEmail(e.target.value)} />
         </div>
         <p>Vorname: {firstname}</p>
         <p>Nachname: {lastname}</p>
