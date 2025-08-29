1b0cf73db93559a50677d721a0afcb41a6131113
AbuTokio <bunyaminxkara@gmail.com>
2025-08-29 15:28:55 +0200

    React - useState - Input - Level 1-6

--- 02-react-usestate/src/App.tsx
+++ 02-react-usestate/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css"
 import Counter from "./pages/counter/Counter"
 import Faq from "./pages/faq/Faq"
+import Input from "./pages/input/Input"
 
 function App() {
   return (
     <>
-      {/* <Faq /> */}
+      <Faq />
       <Counter />
+      <Input />
     </>
   )
 }
---
+++ 02-react-usestate/src/pages/input/Input.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+export default function Input() {
+  const [firstname, setFirstname] = useState<string>("")
+  const [lastname, setLastname] = useState<string>("")
+  const [email, setEmail] = useState<string>("")
+  return (
+    <>
+      <div className="flex justify-between items-start flex-col gap-8 w-[30vw] p-8 border bg-gray-500">
+        <div className="flex justify-center items-center gap-4 w-full">
+          <input className="bg-white p-2" type="text" onChange={(e) => setFirstname(e.target.value)} />
+          <input className="bg-white p-2" type="text" onChange={(e) => setLastname(e.target.value)} />
+          <input className="bg-white p-2" type="email" onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <p>Vorname: {firstname}</p>
+        <p>Nachname: {lastname}</p>
+        <p>Email: {email}</p>
+      </div>
+    </>
+  )
+}
